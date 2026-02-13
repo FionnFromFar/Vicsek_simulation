732136bf7452076aed8f711cc8745f9a0c5068b6
Fionn Doheny <fionndoheny73@gmail.com>
2026-02-13 15:42:58 +0000

    New version with added kinematics

--- particle.py
+++ particle.py
@@ -1,10 +1,25 @@
 class particle:
-    def __init__(self, x, y):
+    def __init__(self, x, y, vx, vy):
         self.x = x
         self.y = y
+        self.vx = vx
+        self.vy = vy
+    
+    def move(self, dt):
+        #addition of function which uses velocity to update position
+        self.x = self.x + (self.vx * dt)
+        self.y = self.y + (self.vy * dt)
     
     def report_position(self):
-        print(f"I am a particle with position x:{self.x}, y:{self.y}")
+        print(f"I am a particle with position x:{self.x:.2f}, y:{self.y:.2f}")
 
-my_bird = particle(5.0, 10.0)
+#testing the new physics with start position 0 and some initial velocity
+my_bird = particle(x=0.0, y=0.0, vx=2.0, vy=1.0) 
+
+print("Time t=0:")
+my_bird.report_position()
+
+my_bird.move(dt=0.5)
+
+print("Time t=0.5:")
 my_bird.report_position()
